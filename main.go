87a74f2077646bb6fac6efd70bbf7e3a1fe23c25
Tom McKeesick <tmck01@gmail.com>
2021-06-05 23:49:21 +1000

    global state demo
    ```
     ☯ ~/d/termux-hardware-stats time ./main
    {"Busy%":{"CPU0":"9","CPU1":"5","CPU2":"5","CPU3":"5","CPU4":"1","CPU5":"28","CPU6":"0","CPU7":"0"}}
    
    real    0m0.016s
    user    0m0.004s
    sys     0m0.013s
    ```

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ var STATS = map[string]string{
 	"curr_freq": "cpufreq/scaling_cur_freq",
 	"min_freq":  "cpufreq/scaling_min_freq",
 	"max_freq":  "cpufreq/scaling_max_freq",
+	"global_state": "core_ctl/global_state",
 }
 
 func check(e error) {
@@ -21,7 +22,7 @@ func check(e error) {
     }
 }
 
-func main() {
+func demo_curr_freq() {
 	for i := 0 ; i < N_CPUS; i++ {
 		core := fmt.Sprintf("%s%d", "cpu", i)
 		dat, err := ioutil.ReadFile(
@@ -37,5 +38,37 @@ func main() {
 		)
 		fmt.Println(string(json))
 	}
+}
+
+func demo_global_state() {
+	dat, err := ioutil.ReadFile(
+		fmt.Sprintf("%s/%s/%s", BASE_DIR, "cpu0", STATS["global_state"]),
+	)
+	check(err)
+	lines := strings.Split(string(dat), "\n")
+	current := ""
+	stat := "Busy%"
+	stats := make(map[string]map[string]string)
+	stats[stat] = make(map[string]string)
+	for _, el := range(lines) {
+		if len(el) == 0 {
+			break
+		}
+		if el[:3] == "CPU" {
+			current = el
+			continue
+		}
+		line := strings.Split(strings.TrimSpace(el), ":")
+		if line[0] == "Busy%" {
+			stats[stat][current] = strings.TrimSpace(line[1])
+		}
+	}
+	json, _ := json.Marshal(stats)
+	fmt.Println(string(json))
+}
+
+func main() {
+	demo_global_state()
+	// demo_curr_freq()
 }
 
