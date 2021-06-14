afcbe359c1e963ae5b8e788885ee35981c90cd1e
Tom McKeesick <tmck01@gmail.com>
2021-06-14 17:55:15 +1000

    boilerplate poc argparser

--- termux_hardware_stats/termux_hardware_stats.py
+++ termux_hardware_stats/termux_hardware_stats.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 
 from dataclasses import dataclass
-from collections import defaultdict, namedtuple
-from itertools import takewhile, repeat
-import multiprocessing
-from functools import lru_cache
 
-from termux_hardware_stats.stats import termux_cpu, termux_mem
+from stats import termux_cpu, termux_mem
+
+import argparse
+
+import ujson as json
+
+def run():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('op', type=str, choices=['cpu', 'mem'], help='which hardware stats to grab')
+
+    args = parser.parse_args()
+
+    if args.op == 'cpu':
+        print(json.dumps(list(termux_cpu.CPUGlobalStateReader(8).load_all())))
+    print(args.accumulate(args.integers))
+
+if __name__ == '__main__':
+    run()
