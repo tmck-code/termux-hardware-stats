8e937cd24aeb2b07046274f55ef62f2ee7cc63dc
Tom McKeesick <tmck01@gmail.com>
2021-06-14 18:15:35 +1000

    All operations supported by cli args

--- termux_hardware_stats/termux_hardware_stats.py
+++ termux_hardware_stats/termux_hardware_stats.py
@@ -8,14 +8,34 @@ import argparse
 
 import ujson as json
 
+
 def run():
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('op', type=str, choices=['cpu', 'mem'], help='which hardware stats to grab')
+    # create the top-level parser
+    parser = argparse.ArgumentParser(prog='Termux Hardware Stats')
+    subparsers = parser.add_subparsers(help='Operation help', dest='hardware')
 
-    args = parser.parse_args()
+    # create the parser for the 'cpu' command
+    parser_a = subparsers.add_parser('cpu', help='CPU help')
+    parser_a.add_argument('op', type=str, choices=['state', 'freqs', 'usage'], help='CPU stat')
 
-    if args.op == 'cpu':
-        print(json.dumps(list(map(asdict, termux_cpu.CPUGlobalStateReader(8).load_all()))))
+    # create the parser for the 'mem' command
+    parser_b = subparsers.add_parser('mem', help='Mem help')
+    parser_b.add_argument('op', type=str, choices=['meminfo', 'meminfo_complete'], help='Mem stat')
+    args = parser.parse_args().__dict__
+
+    if args['hardware'] == 'cpu':
+        if args['op'] == 'state':
+            print(json.dumps(list(map(asdict, termux_cpu.CPUGlobalStateReader(8).load_all()))))
+        if args['op'] == 'usage':
+            print(json.dumps(list(termux_cpu.CPUGlobalStateReader(8).load_percentages())))
+        elif args['op'] == 'freqs':
+            print(json.dumps(list(map(asdict, termux_cpu.CPUFrequencyReader(8).load_all()))))
+    elif args['hardware'] == 'mem':
+        if args['op'] == 'meminfo':
+            print(json.dumps(asdict(termux_mem.MemInfoReader().load())))
+        if args['op'] == 'meminfo_complete':
+            print(json.dumps(asdict(termux_mem.MemInfoReader().load_all())))
 
 if __name__ == '__main__':
     run()
+
