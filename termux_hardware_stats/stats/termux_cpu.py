6c78ee616a8edc4de268af40b5d3ba2f6f79e712
Tom McKeesick <tmck01@gmail.com>
2021-06-14 17:45:13 +1000

    Add test for cpu frequencies

--- termux_hardware_stats/stats/termux_cpu.py
+++ termux_hardware_stats/stats/termux_cpu.py
@@ -1,6 +1,7 @@
-from collections import namedtuple
+from collections import defaultdict, namedtuple
 from itertools import takewhile
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, asdict, field
+from os import strerror
 
 FREQ_KEYS = ['curr_freq', 'min_freq', 'max_freq']
 
@@ -48,12 +49,26 @@ class CPUGlobalStateReader:
         return [core.BusyPercentage for core in self.load_all()]
 
 @dataclass
+class CPUFrequencyFpaths:
+    current: str = FPATHS['curr_freq']
+    min:     str = FPATHS['min_freq']
+    max:     str = FPATHS['max_freq']
+
+    def for_core(self, n):
+        return [s.format(n) for s in [self.current, self.min, self.max]]
+
+    def __iter__(self):
+        yield from [self.current, self.min, self.max]
+
+@dataclass
 class CPUFrequencyReader:
     cpu_count: int
+    fpaths:    CPUFrequencyFpaths = field(default_factory=CPUFrequencyFpaths)
 
-    def load_for_core(n):
-        return [int(next(open(FPATHS[k].format(n)))) for k in FREQ_KEYS]
+    @staticmethod
+    def load_for_core(fpaths):
+        return [int(next(open(fpath))) for fpath in fpaths]
 
     def load_all(self):
         for i in range(self.cpu_count):
-            yield CPUFrequency(*CPUFrequencyReader.load_for_core(i))
+            yield CPUFrequency(*CPUFrequencyReader.load_for_core(self.fpaths.for_core(i)))
--- test/test_termux_cpu.py
+++ test/test_termux_cpu.py
@@ -1,120 +1,65 @@
 from termux_hardware_stats.stats import termux_cpu
 from dataclasses import asdict
 import os
+from glob import glob
 
 FILE_DATA = '''\
 CPU0
-	CPU: 0
-	Online: 1
-	Isolated: 0
-	First CPU: 0
-	Busy%: 56
-	Is busy: 1
-	Not preferred: 0
-	Nr running: 11
-	Active CPUs: 4
-	Need CPUs: 4
-	Nr isolated CPUs: 0
-	Boost: 0
-	OPBoost: 0
+    CPU: 0
+    Online: 1
+    Isolated: 0
+    First CPU: 0
+    Busy%: 56
+    Is busy: 1
+    Not preferred: 0
+    Nr running: 11
+    Active CPUs: 4
+    Need CPUs: 4
+    Nr isolated CPUs: 0
+    Boost: 0
+    OPBoost: 0
 CPU1
-	CPU: 1
-	Online: 1
-	Isolated: 0
-	First CPU: 0
-	Busy%: 78
-	Is busy: 1
-	Not preferred: 0
-	Nr running: 11
-	Active CPUs: 4
-	Need CPUs: 4
-	Nr isolated CPUs: 0
-	Boost: 0
-	OPBoost: 0
+    CPU: 1
+    Online: 1
+    Isolated: 0
+    First CPU: 0
+    Busy%: 78
+    Is busy: 1
+    Not preferred: 0
+    Nr running: 11
+    Active CPUs: 4
+    Need CPUs: 4
+    Nr isolated CPUs: 0
+    Boost: 0
+    OPBoost: 0
 CPU2
-	CPU: 2
-	Online: 1
-	Isolated: 0
-	First CPU: 0
-	Busy%: 100
-	Is busy: 1
-	Not preferred: 0
-	Nr running: 11
-	Active CPUs: 4
-	Need CPUs: 4
-	Nr isolated CPUs: 0
-	Boost: 0
-	OPBoost: 0
+    CPU: 2
+    Online: 1
+    Isolated: 0
+    First CPU: 0
+    Busy%: 100
+    Is busy: 1
+    Not preferred: 0
+    Nr running: 11
+    Active CPUs: 4
+    Need CPUs: 4
+    Nr isolated CPUs: 0
+    Boost: 0
+    OPBoost: 0
 CPU3
-	CPU: 3
-	Online: 1
-	Isolated: 0
-	First CPU: 0
-	Busy%: 43
-	Is busy: 1
-	Not preferred: 0
-	Nr running: 11
-	Active CPUs: 4
-	Need CPUs: 4
-	Nr isolated CPUs: 0
-	Boost: 0
-	OPBoost: 0
-CPU4
-	CPU: 4
-	Online: 1
-	Isolated: 0
-	First CPU: 4
-	Busy%: 84
-	Is busy: 1
-	Not preferred: 0
-	Nr running: 9
-	Active CPUs: 3
-	Need CPUs: 3
-	Nr isolated CPUs: 0
-	Boost: 0
-	OPBoost: 0
-CPU5
-	CPU: 5
-	Online: 1
-	Isolated: 0
-	First CPU: 4
-	Busy%: 100
-	Is busy: 1
-	Not preferred: 0
-	Nr running: 9
-	Active CPUs: 3
-	Need CPUs: 3
-	Nr isolated CPUs: 0
-	Boost: 0
-	OPBoost: 0
-CPU6
-	CPU: 6
-	Online: 1
-	Isolated: 0
-	First CPU: 4
-	Busy%: 100
-	Is busy: 1
-	Not preferred: 0
-	Nr running: 9
-	Active CPUs: 3
-	Need CPUs: 3
-	Nr isolated CPUs: 0
-	Boost: 0
-	OPBoost: 0
-CPU7
-	CPU: 7
-	Online: 1
-	Isolated: 0
-	First CPU: 7
-	Busy%: 100
-	Is busy: 1
-	Not preferred: 0
-	Nr running: 3
-	Active CPUs: 1
-	Need CPUs: 1
-	Nr isolated CPUs: 0
-	Boost: 0
-	OPBoost: 0
+    CPU: 3
+    Online: 1
+    Isolated: 0
+    First CPU: 0
+    Busy%: 43
+    Is busy: 1
+    Not preferred: 0
+    Nr running: 11
+    Active CPUs: 4
+    Need CPUs: 4
+    Nr isolated CPUs: 0
+    Boost: 0
+    OPBoost: 0
 '''
 
 class TestTermuxCPU:
@@ -127,6 +72,8 @@ class TestTermuxCPU:
     def teardown_class(cls):
         if os.path.exists('test/global_state.txt'):
             os.remove('test/global_state.txt')
+        for fpath in glob('test/core_*'):
+            os.remove(fpath)
 
     def test_global_state(self):
         expected = [
@@ -150,10 +97,26 @@ class TestTermuxCPU:
         assert all(isinstance(el, termux_cpu.GlobalState) for el in result)
         assert asdict(result[0]) == expected[0]
 
-    def test_load_usage(self):
-        expected = [56, 78, 100, 43, 84, 100, 100, 100]
-        result = termux_cpu.CPUGlobalStateReader('test/global_state.txt', 8).load()
+    def test_core_usage(self):
+        expected = [56, 78, 100, 43]
+        result = termux_cpu.CPUGlobalStateReader('test/global_state.txt', 4).load_percentages()
         assert result == expected
 
-    def test_load_frequencies(self):
-        pass
+    def test_core_frequencies(self):
+        fpaths = [
+            'test/core_{:d}_current',
+            'test/core_{:d}_min',
+            'test/core_{:d}_max',
+        ]
+        freqs = [
+            {'current': 3500000, 'min': 18000, 'max': 3700000},
+            {'current': 3000000, 'min': 20000, 'max': 3800000},
+        ]
+        for i, f in enumerate(freqs):
+            for j, (key, data) in enumerate(f.items()):
+                with open(fpaths[j].format(i), 'w') as ostream:
+                    ostream.write(f'{data}\n')
+        result = list(termux_cpu.CPUFrequencyReader(2, termux_cpu.CPUFrequencyFpaths(*fpaths)).load_all())
+        assert all(isinstance(el, termux_cpu.CPUFrequency) for el in result)
+        for i, r in enumerate(result):
+            assert r == termux_cpu.CPUFrequency(**freqs[i])
